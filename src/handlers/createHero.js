13f36b2377b5e8a7d46d62c5a8bade205e475774
Jeferson Narváez <alexresidentevil@gmail.com>
2021-07-16 10:36:48 -0500

    Setting role statements to Put an item in dynamodb

--- src/handlers/createHero.js
+++ src/handlers/createHero.js
@@ -1,19 +1,31 @@
-async function createHero(event, context) { //Event and lambda is provided by AWS when lambda is called
-  const {heroName,superPower} =JSON.parse(event.body)
-  const now=new Date();
-  const auction={
+import { v4 as uuid } from "uuid";
+import AWS from "aws-sdk";
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+async function createHero(event, context) {
+  //Event and lambda is provided by AWS when lambda is called
+  const { heroName, superPower } = JSON.parse(event.body);
+  const now = new Date();
+  const hero = {
+    id: uuid(),
     heroName,
     superPower,
-    status:'OPEN',
-    createdAt:now.toISOString(),
-  }
+    status: "OPEN",
+    createdAt: now.toISOString(),
+  };
+
+  await dynamodb
+    .put({
+      TableName: "HerosTable",
+      Item: hero,
+    })
+    .promise();
+
   return {
     statusCode: 201,
-    body: JSON.stringify(auction),
+    body: JSON.stringify(hero),
   };
 }
 
 export const handler = createHero;
-
-
-
